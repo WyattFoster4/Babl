5106745fe62bc509de8744f7054a4365ae1ad41f
Luke Dreyfuss <123597704+ldreyfuss@users.noreply.github.com>
2023-01-26 20:27:56 +0000

    updated js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let guessingButton = document.getElementById("guessingButton");
 
 // Enter key = clicking "guess"
 guessingBox.addEventListener("keyup", function(event) {
-  guessingButton.addEventListener("click", () => guess());
+  guessingButton.addEventListener("click", () => guessingFunction());
   if (event.keyCode === 13) {
     guessingButton.click();
   }
@@ -38,7 +38,7 @@ function compareLanguages(guess, correct) {
                 let guessCorrect = data.languageData.languages.find(lang => (lang.language1 === guess && lang.language2 === correct) || (lang.language1 === correct && lang.language2 === guess));
                 if (guessCorrect) {
                     similarityScore = guessCorrect.distance;
-                    resolve(similarityScore);
+                    return resolve(similarityScore);
                 } else {
                     reject("Similarity score not found");
                 }
@@ -46,9 +46,6 @@ function compareLanguages(guess, correct) {
             .catch(error => reject(error));
     });
 }
-
-// Runs language check
-
 
 // Empty the guessing boxes
 document.getElementById("guess-1").innerHTML = "";
@@ -58,10 +55,11 @@ document.getElementById("guess-4").innerHTML = "";
 document.getElementById("guess-5").innerHTML = "";
 document.getElementById("guess-6").innerHTML = "";
 
-function calculateProx(arrInput,lang) {
-  compareLanguages(arrInput, lang)
+async function calculateProx(arrInput,lang) {
+  return compareLanguages(arrInput, lang)
       .then(distance => {
-          return (100 - distance) + "%";
+          let proximity = (100 - distance) + "%";
+          printPercent(proximity);
       })
       .catch(error => console.error(error)); 
 }
@@ -69,17 +67,17 @@ function calculateProx(arrInput,lang) {
 // Function sends guess to HTML
 function printGuess(guesses) {
   if (guesses.length === 1) {
-    document.getElementById("guess-1").innerHTML = guesses[1];
+    document.getElementById("guess-1").innerHTML = guesses[0];
   } else if (guesses.length === 2) {
-    document.getElementById("guess-2").innerHTML = guesses[2];
+    document.getElementById("guess-2").innerHTML = guesses[1];
   } else if (guesses.length === 3) {
-    document.getElementById("guess-3").innerHTML = guesses[3];
+    document.getElementById("guess-3").innerHTML = guesses[2];
   } else if (guesses.length === 4) {
-    document.getElementById("guess-4").innerHTML = guesses[4];
+    document.getElementById("guess-4").innerHTML = guesses[3];
   } else if (guesses.length === 5) {
-    document.getElementById("guess-5").innerHTML = guesses[5];
+    document.getElementById("guess-5").innerHTML = guesses[4];
   } else if (guesses.length === 6) {
-    document.getElementById("guess-6").innerHTML = guesses[6];
+    document.getElementById("guess-6").innerHTML = guesses[5];
   } 
 }
 
@@ -105,21 +103,25 @@ document.getElementById("popHeading").innerHTML = "Welcome to Babl!";
 document.getElementById("popText").innerHTML = "A twist on the traditional Wordle, you'll test out your linguistic skills through a daily guessing puzzle. Here's how to play: Every day, a new phrase in a foreign language will appear on Babl. You have to guess what language the phrase is written in (it doesn't matter what the phrase actually says). If your guess is right, you'll win! If your guess is wrong, we'll tell you how close you got. You only get 6 tries. Good luck!";
 modal.showModal();
 
-
 // Event listener collects values from guessing box
 function guessingFunction() {
   let arrInput = guessingBox.value;
   if (arrInput !== "" && arrInput !== " ") {
-    if (guesses.length < 6 && won === false) {
-      guesses.push(arrInput);
-      printGuess(guesses);
-      printPercent(calculateProx(arrInput,lang));
-      if (arrInput === lang) {
-        won = true;
-        document.getElementById("popHeading").innerHTML = "You won!";
-        document.getElementById("popText").innerHTML = "You're a language genius! Come back tomorrow for the next puzzle.";
-        modal.showModal();
+    if (data.hasOwnProperty(arrInput)) {
+      if (guesses.length < 6 && won === false) {
+        guesses.push(arrInput);
+        printGuess(guesses);
+        printPercent(calculateProx(arrInput,lang));
+        if (arrInput === lang) {
+          won = true;
+          document.getElementById("popHeading").innerHTML = "You won!";
+          document.getElementById("popText").innerHTML = "You're a language genius! Come back tomorrow for the next puzzle.";
+          modal.showModal();
+        }
       }
+    } else {
+      document.getElementById("popHeading").innerHTML = "Uh-oh!";
+      document.getElementById("popText").innerHTML = "You entered a language that doesn't exist. Make sure that the first letter is capitalized. If that doesn't work, check out this list of accepted languages.";
     }
     if (guesses.length === 6 && won === false) {
       document.getElementById("popHeading").innerHTML = "You lost!";
@@ -134,10 +136,9 @@ function guessingFunction() {
 closeModal.addEventListener('click', () => {
   modal.close();
 });
+
 helpButton.addEventListener('click', () => {
   document.getElementById("popHeading").innerHTML = "Welcome to Babl!";
   document.getElementById("popText").innerHTML = "A twist on the traditional Wordle, you'll test out your linguistic skills through a daily guessing puzzle. Here's how to play: Every day, a new phrase in a foreign language will appear on Babl. You have to guess what language the phrase is written in (it doesn't matter what the phrase actually says). If your guess is right, you'll win! If your guess is wrong, we'll tell you how close you got. You only get 6 tries. Good luck!";
   modal.showModal();
 });
-
-  
