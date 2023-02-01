8826ab698ae0d2ea3b4cc05a78a26ded6778d0bf
gliggy <leocfitz@gmail.com>
2023-02-01 01:44:54 +0000

    fixed async functions

--- script.js
+++ script.js
@@ -18,93 +18,81 @@ let guessingButton = document.getElementById("guessingButton");
 let validLangs = ["Abkhaz", "Adyghe", "Afar", "Afrikaans", "Ainu", "Gheg Albanian", "Tosk Albanian", "Aleut", "Altai", "Amharic", "Arabic", "Armaic", "Arbanaski", "Armenian", "Eastern Albanian", "Assamese", "Avar", "Azeri", "Balochi", "Basaa", "Bashkir", "Basque", "Belarusian", "Bemba", "Bengali", "Bihari", "Bole", "Brahui", "Brazilian", "Breton", "Bulgarian", "Burmese", "Burushaski", "Buryat", "Buyang", "Catalan", "Cebuano", "Chechen", "Cantonese", "Mandarin", "Chuvash", "Comorian", "Cornish", "Croatian", "Czech", "Danish", "Dargwa", "Dhivehi", "Digor Ossetic", "Dizi", "Douala", "Dutch", "English", "Estonian", "Even", "Faroese", "Finnish", "Flemish", "French", "Frisian", "Friulian", "Fula", "Galician", "Gelao", "Georgian", "German", "Greek", "Eastern Greenlandic", "Western Greenlandic", "Gujarati", "Gypsy Romani", "Hausa", "Hebrew", "Hindi", "Hungarian", "Icelandic", "Igbo", "Indonesian", "Irish", "Iron Ossetic", "Ishkashimi", "Istro-Romanian", "Italian", "Japanese", "Javanese", "Kabardian", "Kabylian", "Kalasha", "Kalmyk", "Kannada", "Karelian", "Kashmiri", "Kazakh", "Ket", "Khanti", "Khmer", "Khowar", "Kivalliq", "Komi", "Korean", "Kurdish", "Kurukh", "Kyrgyz", "Labrador Inuttut", "Ladin", "Lahnda", "Lak", "Lao", "Latvian", "Letzebuergesch", "Lezgian", "Lithuanian", "Macedonian", "Magahi", "Malagasy", "Malayalam", "Maltese", "Mansi", "Maori", "Marathi", "Mari", "Marwari", "Mon", "Mongolian", "Nayi", "Nenets", "Nepali", "Norwegian Bokmal", "Norwegian Nynorsk", "Oriya", "Oromo", "Oroqen", "Pashto", "Pennsylvania Dutch", "Persian", "Polish", "Portuguese", "Provencal", "Punjabi", "Romanian", "Romansch", "Russian", "Sami", "Samoan", "Sardinian Logudorese", "Sardinian Nuorese", "Sariqoli", "Schwyzerduetsch", "Scots", "Scottish Gaelic", "Serbian", "Shan", "Sheko", "Sindhi", "Sinhalese", "Slovak", "Slovene", "Somali", "Spanish", "Sranan", "Swahili", "Swedish", "Tagalog", "Tahitian", "Tajik", "Tamasheq", "Tamil", "Tashelhit", "Tatar", "Tausug", "Telugu", "Thai", "Tibetan", "Tigrigna", "Tmazight", "Tsakonian", "Turkish", "Turkmen", "Tuvan", "Ukrainian", "Urdu", "Uyghur", "Uzbek", "Veps", "Vietnamese", "Vlach", "Wakhi", "Walloon", "Warji", "Waziri", "Welsh", "Wolof", "Yakut", "Yiddish", "Yoruba", "Yupik", "Zazaki", "Zulu"]; // Verifies languages
 
 // HTML & CSS SETUP
-document.getElementById("guess-1").innerHTML = ""; // Clears guessing boxes at start
-document.getElementById("guess-2").innerHTML = "";
-document.getElementById("guess-3").innerHTML = "";
-document.getElementById("guess-4").innerHTML = "";
-document.getElementById("guess-5").innerHTML = "";
-document.getElementById("guess-6").innerHTML = "";
+for (var i = 1; i<7; i++) {
+  document.getElementById("guess-"+i).innerHTML = ""; // Clears guessing boxes at start
+}
 
 document.getElementById("percent-1").style.background = "#808080";
 
 // FUNCTIONS SETUP
-function compareLanguages(guess, correct) {
-  fetch("data.json")
-        .then(response => response.json())
-        .then(data => {
-            let similarityScore;
-            let guessCorrect = data.languageData.languages.find(lang => (lang.language1 === guess && lang.language2 === correct) || (lang.language1 === correct && lang.language2 === guess));
-            if (guessCorrect) {
-                similarityScore = guessCorrect.distance;
-                callback(null, similarityScore);
-            } else {
-                callback("Similarity score not found", null);
-            }
-        })
-        .catch(error => callback(error, null));
+async function compareLanguages(guess, correct) {
+   let data = await fetch("./data.json", { method: 'GET' }).then(response => response.json(response))
+  console.log(data)
+  // data.languageData.languages.find(guess)
+  // console.log(typeof(data))
+  let similarityScore;
+  let guessCorrect = await data.languageData.languages.find(lang => (lang.language1 === guess && lang.language2 === correct) || (lang.language1 === correct && lang.language2 === guess));
+  console.log(guessCorrect)
+  similarityScore = guessCorrect.distance;
+  return 100 - parseInt(similarityScore);
 }
-
-function calculateProx(arrInput, lang, callback) {
-  compareLanguages(arrInput, lang, (error, distance) => {
-    if (error) {
-      callback(error, null);
-    } else {
-      let proximity = (100 - distance);
-      callback(null, proximity);
-    }
-  });
-}
+/*
+async function calculateProx(arrInput, lang) {
+  //console.log(await compareLanguages(arrInput, lang), " prox")
+  return 100 - await compareLanguages(arrInput, lang)
+}*/
 
 function printGuess(guesses) { // Sends guess to HTML
-  document.getElementById("guess-" + guesses.length).innerHTML = guesses[guesses.length];
+  document.getElementById("guess-" + guesses.length).innerHTML = guesses[guesses.length - 1];
 }
-  
+
 function printPercent(proximity) { // Sends percent and colors to HTML & CSS
   console.log(proximity)
-  const colors = ["#ff4d3d","#ff6a3d","#ff9b3d","#ffcf3d","#fff53d","#e2ff3d","#b5ff3d","#21ff3b"];
+  const colors = ["#ff4d3d", "#ff6a3d", "#ff9b3d", "#ffcf3d", "#fff53d", "#e2ff3d", "#b5ff3d", "#21ff3b"];
   const checkpoints = [12.5, 25, 37.5, 50, 62.5, 75, 87.5, 100];
-  sendColor = "";
-  let i = 0;
-  while (sendColor == "") {
+  var sendColor = 'a';
+
+  for (var i = 0; i<checkpoints.length; i++) {
     if (proximity < checkpoints[i]) {
       sendColor = colors[i];
     }
-    i++;
   }
+  
   document.getElementById("percent-" + guesses.length).innerHTML = proximity + "%";
   document.getElementById("percent-" + guesses.length).style.background = sendColor;
 }
 
 //Tells you if the answer is right or wrong
-function guessingFunction() {
+async function guessingFunction() {
   let arrInput = guessingBox.value;
-  Console.log("Omg it's actually calling guessingFunction");
+  console.log("Omg it's actually calling guessingFunction");
   if (!validLangs.includes(arrInput)) {
     document.getElementById("popHeading").innerHTML = "Uh-oh!";
     document.getElementById("popText").innerHTML = "You entered a language that doesn't exist. Make sure that the first letter is capitalized. If that doesn't work, check out this list of accepted languages.";
     modal.showModal();
-    return
+    return;
   }
-    if (guesses.length < 5 && won == false && arrInput != lang) {
-      guesses.push(arrInput);
-      printGuess(guesses);
-      let proximity = calculateProx(arrInput,lang);
-      printPercent(proximity);
-    } else if (guesses.length <= 6 && won == false && arrInput == lang) {
-      won = true;
-      guesses.push(arrInput);
-      printGuess(guesses);
-      printPercent(calculateProx(arrInput,lang));
-      document.getElementById("popHeading").innerHTML = "You won!";
-      document.getElementById("popText").innerHTML = "You're a language genius! Come back tomorrow for the next puzzle.";
-      modal.showModal();
-    } else if (guesses.length == 5 && won == false && arrInput != lang) {
-      guesses.push(arrInput);
-      printGuess(guesses);
-      printPercent(calculateProx(arrInput,lang));
-      document.getElementById("popHeading").innerHTML = "You lost!";
-      document.getElementById("popText").innerHTML = "You didn't guess the correct language. Come back tomorrow for the next puzzle.";
-      modal.showModal();
+  if (guesses.length < 5 && won == false && arrInput != lang) {
+    guesses.push(arrInput);
+    printGuess(guesses);
+    let proximity = await compareLanguages(arrInput, lang);
+    console.log(proximity + "proximity")
+    printPercent(proximity);
+  } else if (guesses.length <= 6 && won == false && arrInput == lang) {
+    won = true;
+    guesses.push(arrInput);
+    printGuess(guesses);
+    printPercent(await compareLanguages(arrInput, lang));
+    document.getElementById("popHeading").innerHTML = "You won!";
+    document.getElementById("popText").innerHTML = "You're a language genius! Come back tomorrow for the next puzzle.";
+    modal.showModal();
+  } else if (guesses.length == 5 && won == false && arrInput != lang) {
+    guesses.push(arrInput);
+    printGuess(guesses);
+    printPercent(await compareLanguages(arrInput, lang));
+    document.getElementById("popHeading").innerHTML = "You lost!";
+    document.getElementById("popText").innerHTML = "You didn't guess the correct language. Come back tomorrow for the next puzzle.";
+    modal.showModal();
   }
   guessingBox.value = "";
 }
@@ -129,3 +117,9 @@ guessingBox.addEventListener("keyup", function(event) {
   }
 });
 guessingButton.addEventListener("click", () => guessingFunction());
+
+
+//automatically scrolls back to the top of the page when refreshed
+window.onunload = function () {
+    window.scrollTo(0,0);
+};
